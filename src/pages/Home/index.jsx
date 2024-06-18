5405862cf41b549bf85e39487b8811ef0592c384
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 17:46:56 -0600

    import Banner component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
 
 function Home() {
     return (
         <>
             <Header />
+            <Banner img='home' color="#154580" />
             <h1>Welcome Home prros!</h1>
             <Footer />
         </>
