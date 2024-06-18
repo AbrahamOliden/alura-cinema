0e4fbaf5a96cefb6b52aa4028c47af528d26a3ad
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 00:31:55 -0600

    import logo in Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,17 @@
-import styles from './index.modules.css'
+import { Link } from 'react-router-dom';
+import styles from './index.modules.css';
+import logo from './logo-alura.png';
 
 function Header() {
   return (
-    <header className={styles.header} ></header>
+    <header className={styles.header}>
+        <Link to='/' >
+            <section className={styles.logoContainer} >
+                <img src={logo} alt="Logo Alura" />
+                <span>Cinema</span>
+            </section>
+        </Link>
+    </header>
   );
 };
 
