025ae7e4067d9de4b4cf9d8cd3c7881341b05362
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 17:14:41 -0600

    create Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from  './footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer} >
+        <h4 className={styles.text} >Developed by Abraham Oliden</h4>
+    </footer>
+  );
+};
+
+export default Footer;
