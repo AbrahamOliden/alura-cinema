1f36b541542e2bb145155ef8c7aedd8e2685c715
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 20:31:33 -0600

    create new route for Favorites page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} ></Route>
+                <Route path='/' element={<Home />} />
+                <Route path="/favorites" element={<Favorites />} />
             </Routes>
         </BrowserRouter>
     );
