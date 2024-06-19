c86a292e8613f25674b0490fc4926d927201b45a
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-19 15:15:37 -0600

    import useFavoritesContext to update favorite videos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,24 @@
+import { useFavoritesContext } from '../../context/Favorites';
 import styles from './Card.module.css';
+import favActive from "/img/favorite 1.png";
+import favInactive from "/img/favorite_outline 1.png";
 
 import React from 'react'
 
 function Card({id, capa: image, titulo: title}) {
+
+  const { favorite, addFavorite } = useFavoritesContext();
+  const isFavorite = favorite.some(fav => fav.id === id);
+  const icon = isFavorite ? favActive : favInactive
+
   return (
     <div className={styles.container}>
         <img src={image} alt={title} className={styles.image} />
         <h2 className={styles.title} >{title}</h2>
-        <img src="img/favorite 1.png" alt="favorite icon" />
+        <img 
+          src={icon} 
+          alt="favorite icon"
+          onClick={() => addFavorite({ id, image, title })} />
     </div>
   );
 };
