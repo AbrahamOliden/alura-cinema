75497072482fc6c1aac4b2704efe32ec3330dedf
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 19:41:28 -0600

    first draft for Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,9 +2,13 @@ import styles from './Card.modules.css';
 
 import React from 'react'
 
-function Card() {
+function Card({id, image, title}) {
   return (
-    <div>Card</div>
+    <div className={styles.container}>
+        <img src={image} alt={title} className={styles.image} />
+        <h2>{title}</h2>
+        <img src="img/favorite 1.png" alt="favorite icon" />
+    </div>
   );
 };
 
