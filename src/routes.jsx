672b92691862352d74dfd4a6e1b742c7064ccc43
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 21:19:38 -0600

    import FavoritesProvider

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import FavoritesProvider from "./context/Favorites";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Footer from "./components/Footer";
@@ -10,10 +11,12 @@ function AppRoutes() {
         <BrowserRouter>
             <Header />
             <Main>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path="/favorites" element={<Favorites />} />
-                </Routes>
+                <FavoritesProvider>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path="/favorites" element={<Favorites />} />
+                    </Routes>
+                </FavoritesProvider>
 
             </Main>
             <Footer />
