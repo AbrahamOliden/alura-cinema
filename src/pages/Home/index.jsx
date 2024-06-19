0c07fe30d4dbd47dfbe0ca0ed6f97a4d45bd8da6
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 20:04:01 -0600

    import db.json and remove hardcoded Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,9 @@ import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import Title from "../../components/Title";
 import Card from "../../components/Card";
+import videos from "../../data/db.json";
+
+import styles from 'index.module.css';
 
 function Home() {
     return (
@@ -10,10 +13,10 @@ function Home() {
             <Header />
             <Banner img='home' color="#154580" />
             <Title text='Un lugar para guardar sus videos favoritos' />
-            <Card 
+            {/* <Card 
                 id={1} 
                 title='Perro' 
-                image='https://api.thecatapi.com/api/images/get?format=src&ype=png'/>
+                image='https://api.thecatapi.com/api/images/get?format=src&ype=png'/> */}
             <Footer />
         </>
     );
