f5c8a8fa690dd5864dfcaa388a4734e60429e514
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-19 17:30:12 -0600

    create logic to display selected video on an iframe

--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -1,14 +1,29 @@
 import styles from "./Player.module.css";
 import Banner from "../../components/Banner";
 import Title from "../../components/Title";
+import { useParams } from "react-router-dom";
+import videos from "../../data/db.json";
 
 function Player() {
-  return (
-    <div>
-        <Banner img="player" color="#58B9AE" />
+    const params = useParams();
+    const video = videos.find(video => video.id === Number(params.id));
+    console.log(video);
+
+    return (
+    <>
+        <Banner img="player" color="#BF008A" />
         <Title text="Player" />
-    </div>
-  );
+        <section className={styles.container} >
+            <iframe className={styles.player}
+                width="100%"
+                src={video.link}
+                title={video.titulo}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen>
+            </iframe>
+        </section>
+    </>
+    );
 };
 
 export default Player;
