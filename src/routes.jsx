1108ad38b6af5b276bd29ee6db900ed5c22401fd
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 20:38:21 -0600

    import Header and Footer and place inside BrowserRouter

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
+import Footer from "./components/Footer";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path="/favorites" element={<Favorites />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     );
 };
