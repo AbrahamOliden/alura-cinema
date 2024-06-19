5c20360ec70c27901da5d83c519641664db6c661
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 21:02:34 -0600

    create Favorites function

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,10 +1,19 @@
+import Banner from "../../components/Banner";
+import Title from "../../components/Title";
+import Card from "../../components/Card";
 import styles from "./Favorites.module.css";
 
 import React from 'react'
 
 function Favorites() {
   return (
-    <div>Favorites</div>
+    <>
+    <Banner img="favoritos" color="#00BF63" />
+    <Title text="Favoritos"/>
+    <Card 
+      capa="https://i.ytimg.com/vi/cixof3jguHU/maxresdefault.jpg" 
+      titulo="Tacos de sesos" />
+    </>
   );
 };
 
