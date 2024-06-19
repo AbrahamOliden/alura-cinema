20d690031133f1c3755836f32943e5170325811d
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 20:16:06 -0600

    map over db and return Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ import Title from "../../components/Title";
 import Card from "../../components/Card";
 import videos from "../../data/db.json";
 
-import styles from 'index.module.css';
+import styles from './index.module.css';
 
 function Home() {
     return (
@@ -13,6 +13,15 @@ function Home() {
             <Header />
             <Banner img='home' color="#154580" />
             <Title text='Un lugar para guardar sus videos favoritos' />
+            <section className={styles.container} >
+                {
+                videos.map(video => {
+                    return <Card {...video}
+                        key={video.id}
+                    />
+                })
+                }
+            </section>
             {/* <Card 
                 id={1} 
                 title='Perro' 
