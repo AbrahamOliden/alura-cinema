1d2fa303ea2915dc753577360f75fc344aa2981c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-18 19:47:00 -0600

    import Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import Title from "../../components/Title";
+import Card from "../../components/Card";
 
 function Home() {
     return (
@@ -9,6 +10,10 @@ function Home() {
             <Header />
             <Banner img='home' color="#154580" />
             <Title text='Un lugar para guardar sus videos favoritos' />
+            <Card 
+                id={1} 
+                title='Perro' 
+                image='https://api.thecatapi.com/api/images/get?format=src&ype=png'/>
             <Footer />
         </>
     );
