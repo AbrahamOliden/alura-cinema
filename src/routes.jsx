2430ef80481eec8c07c8b2e615a4ad0e3ed9b19f
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-19 18:43:17 -0600

    create main Route and nest all other Routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,29 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import FavoritesProvider from "./context/Favorites";
+import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Player from "./pages/Player";
 import NotFound from "./pages/NotFound";
-import Footer from "./components/Footer";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Header />
-            <Main>
-                <FavoritesProvider>
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path="/favorites" element={<Favorites />} />
-                        <Route path="/:id" element={<Player />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </FavoritesProvider>
-
-            </Main>
-            <Footer />
+            <Routes>
+                <Route path="/" element={<LandingPage />} >
+                    <Route index element={<Home />} />
+                    <Route path="favorites" element={<Favorites />} />
+                    <Route path=":id" element={<Player />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     );
 };
