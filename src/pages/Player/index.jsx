cabcb23dc243902373c360fd0c4012ab0c54fa7b
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-19 18:24:44 -0600

    import NotFound component as a fallbock if url doesnt exist

--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -1,13 +1,15 @@
+import { useParams } from "react-router-dom";
 import styles from "./Player.module.css";
 import Banner from "../../components/Banner";
 import Title from "../../components/Title";
-import { useParams } from "react-router-dom";
 import videos from "../../data/db.json";
+import NotFound from "../NotFound";
 
 function Player() {
     const params = useParams();
     const video = videos.find(video => video.id === Number(params.id));
-    console.log(video);
+
+    if (!video) return <NotFound />
 
     return (
     <>
